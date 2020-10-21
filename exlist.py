00e9e0bffcd048d5df354c970622a510a2044af0
Wloha <72493130+Wloha@users.noreply.github.com>
2020-10-21 10:52:48 +0700

    Delete exlist.py

--- exlist.py
+++
@@ -1,9 +0,0 @@
-import csv
-l = dict()
-with open('listname.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    line_count = 0
-
-    for row in csv_reader:
-        l[row['name']]= 0
-
